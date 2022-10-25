d5719f24b92d1c67a3c567ec620d2e1ed0584a18
Vitor Nunes Oliveira <kira18nunes19@gmail.com>
2022-10-25 00:11:08 -0300

    terminando caso de teste tarefa
    mapeando elementos e criando os métodos com as ações  do caso de teste tarefas

--- src/test/java/com/javaseleniumtemplate/pages/TaskPage.java
+++ src/test/java/com/javaseleniumtemplate/pages/TaskPage.java
@@ -6,15 +6,45 @@ import org.openqa.selenium.By;
 public class TaskPage extends PageBase {
     //MAPPING
     By TaskCreatefield = By.xpath("//*[@id=\"navbar-container\"]/div[2]/ul/li[1]/div/a");
-    By resumefield = By.id("summary");
+    By resumeField = By.id("summary");
     By descriptionField = By.id("description");
     By createNewField = By.xpath("//*[@id=\"report_bug_form\"]/div/div[2]/div[2]/input");
     By deleteTaskField = By.xpath("//*[@id=\"main-container\"]/div[2]/div[2]/div/div[1]/div/div[2]/div[2]/div/table/tfoot/tr/td/div/div[10]/form/fieldset/input[4]");
     By deleteTaskConfirm = By.xpath("//*[@id=\"action-group-div\"]/form/div/div[2]/div[2]/input");
-    By filterField = By.id("filter-search-txt");
-    By filterAplyfield = By.xpath("//*[@id=\"filters_form_open\"]/div[2]/div/div/input[2]");
-    By counterTaskfield = By.xpath("//*[@id=\"bug_action\"]/div/div[1]/h4/span");
+    By viewTasks = By.xpath("//*[@id=\"sidebar\"]/ul/li[2]/a/i");
+    By filterField = By.id("filter-search-txt"); //escrever o nome da tarefa
+    By filterAplyfield = By.xpath("//*[@id=\"filters_form_open\"]/div[2]/div/div/input[2]");//clicar
+    By counterTaskfield = By.xpath("//*[@id=\"bug_action\"]/div/div[1]/h4/span");//clicar
 
     //ACTIONS
-
+    public void clicarEmCriarTarefa(){
+        click(TaskCreatefield);
+    }
+    public void preencherResumo(String Resumo){
+        sendKeys(resumeField, Resumo);
+    }
+    public void preencherDescricao(String descricao){
+        sendKeys(descriptionField, descricao);
+    }
+    public void clicarEmFinalizar(){
+        click(createNewField);
+    }
+    public void clicarEmDeletar(){
+        click(deleteTaskField);
+    }
+    public void clicarEmConfirmar(){
+        click(deleteTaskConfirm);
+    }
+    public void clicarEmVerTarefas(){
+        click(viewTasks);
+    }
+    public void preencherfiltro(String Filtro){
+        sendKeys(filterField, Filtro);
+    }
+    public void clicarEmAplicarFiltro(){
+        click(filterAplyfield);
+    }
+    public String retornaNumeroDeTarefas(){
+        return getText(counterTaskfield);
+    }
 }
--- src/test/java/com/javaseleniumtemplate/tests/TaskTest.java
+++ src/test/java/com/javaseleniumtemplate/tests/TaskTest.java
@@ -1,5 +1,35 @@
 package com.javaseleniumtemplate.tests;
 import com.javaseleniumtemplate.bases.TestBase;
+import com.javaseleniumtemplate.pages.LoginPage;
+import com.javaseleniumtemplate.pages.TaskPage;
+import org.junit.Test;
 
-public class TaskTest {
+public class TaskTest extends TaskPage {
+    //OBJECTS
+    TaskPage taskPage;
+
+    //Tests
+    @Test
+    public void deletarTarefaCriada(){
+
+        //Objects instances
+        taskPage = new TaskPage();
+
+        //parameters
+        String Resumo = "teste automatizado";
+        String descricao = "verificar se tarefa foi excluida";
+        String Filtro = "teste automatizado";
+
+        //Tests
+        taskPage.clicarEmCriarTarefa();
+        taskPage.preencherResumo(Resumo);
+        taskPage.preencherDescricao(descricao);
+        taskPage.clicarEmFinalizar();
+        taskPage.clicarEmDeletar();
+        taskPage.clicarEmConfirmar();
+        taskPage.clicarEmVerTarefas();
+        taskPage.preencherfiltro(Filtro);
+        taskPage.clicarEmAplicarFiltro();
+        taskPage.retornaNumeroDeTarefas()
+    }
 }
